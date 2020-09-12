728ca1d14f0f335742ea1f28dcb869b8759996dd
sajid97 <56666435+sajid97@users.noreply.github.com>
2020-09-11 18:09:06 -0600

    Add files via upload

---
+++ CPSC1012-Exercise01-SajidIslam/CPSC1012-Exercise01-SajidIslam/Program.cs
@@ -0,0 +1,16 @@
+﻿//purpose: learning first code
+//auithor: Sajid Islam
+//last modified date: sept 11, 2020
+
+using System;
+
+namespace CPSC1012_Exercise01_SajidIslam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi All, My name is Sajid Islam. I am a DMIT student and I am doing my third semester and I love photography!");
+        }
+    }
+}
